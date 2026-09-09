5bc45165a4d55900980011c3ea74963f3a5342d3
Kevin Brewster <kbrewster81@gmail.com>
2026-09-08 21:27:56 -0400

    First commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,29 @@
-import React from "react";
-import movieReel from "../assets/movie-reel.jpg";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import movieReel from "./assets/movie-reel.jpg";
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/popular?api_key=5b53b7f6f16e2001e5418298c990bf2a",
+      )
+      .then((res) => setMovies(res.data.results))
+      .catch((err) => console.error(err));
+  }, []);
+
+  const handleMovieClick = (id) => {
+    navigate(`/movie/${id}`);
+  };
+
   return (
     <header>
       <div>
-        <img className="background-image" src={movieReel} alt="" />
+        <img className="background-image" src={movieReel.jpg} alt="" />
       </div>
       <div className="header__container">
         <div className="header__description">
